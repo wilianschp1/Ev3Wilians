c87330aa809c9829e2dce4f1f4fe9d3028d54ba4
wilianschp1 <90275507+wilianschp1@users.noreply.github.com>
2023-07-24 19:18:19 +0000

    commit5

--- src/main/java/com/example/demo/InstrumentoRepository.java
+++ src/main/java/com/example/demo/InstrumentoRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
-public interface InstrumentoRepository extends CrudRepository<Instrumento, Integer> {
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
+public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>,CrudRepository<Instrumento, Integer> {
+
+    List<Instrumento> findByNombre(@Param("nombre") String nombre);
+
 }
